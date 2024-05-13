01b4afe642142224a5009012103f5c96dd59ea2c
AnnSofie <annsofie.sjolander@gmail.com>
2024-05-13 12:48:21 +0200

    updated program

--- NewsletterProvider/Program.cs
+++ NewsletterProvider/Program.cs
@@ -1,18 +1,35 @@
 using Data.Contexts;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices((context, services) =>
+    .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        services.AddDbContext<DataContext>(x => x.UseSqlServer(context.Configuration.GetConnectionString("SqlServer")));
+        services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("SqlServer")));
     })
     .Build();
+
+//using (var scope = host.Services.CreateScope())            //onödigt? Hans pratar om detta i skicka och vaildera verifikationskoder (med servicebus) om det inte går att göra update databas med Enviroment
+//{
+//    try
+//    {
+//        var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+//        var migration = context.Database.GetPendingMigrations();
+//        if (migration != null && migration.Any())
+//        {
+//            context.Database.Migrate();
+//        }
+//    }
+//    catch (Exception ex)
+//    {
+//        Debug.WriteLine($"Error : Program, Migration of database :: { ex.Message}");
+//    }  
+//}
 
 host.Run();
