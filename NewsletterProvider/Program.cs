aaf0230d0cae397bee323e2ad19c377eb94dde0b
AnnSofie <annsofie.sjolander@gmail.com>
2024-05-13 11:15:51 +0200

    updated program

--- NewsletterProvider/Program.cs
+++ NewsletterProvider/Program.cs
@@ -1,16 +1,17 @@
 using Data.Contexts;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+    .ConfigureServices((context, services) =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("SqlServer")));
+        services.AddDbContext<DataContext>(x => x.UseSqlServer(context.Configuration.GetConnectionString("SqlServer")));
     })
     .Build();
 
