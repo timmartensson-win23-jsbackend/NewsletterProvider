afb00a16fdce3576575200d0b9f32a586d8a4d41
AnnSofie <annsofie.sjolander@gmail.com>
2024-05-13 13:07:27 +0200

    updated program

--- NewsletterProvider/Program.cs
+++ NewsletterProvider/Program.cs
@@ -15,21 +15,24 @@ var host = new HostBuilder()
     })
     .Build();
 
-//using (var scope = host.Services.CreateScope())            //onödigt? Hans pratar om detta i skicka och vaildera verifikationskoder (med servicebus) om det inte går att göra update databas med Enviroment
-//{
-//    try
-//    {
-//        var context = scope.ServiceProvider.GetRequiredService<DataContext>();
-//        var migration = context.Database.GetPendingMigrations();
-//        if (migration != null && migration.Any())
-//        {
-//            context.Database.Migrate();
-//        }
-//    }
-//    catch (Exception ex)
-//    {
-//        Debug.WriteLine($"Error : Program, Migration of database :: { ex.Message}");
-//    }  
-//}
+using (var scope = host.Services.CreateScope())            //onödigt? Hans pratar om detta i skicka och vaildera verifikationskoder (med servicebus) om det inte går att göra update databas med Enviroment
+{
+    try
+    {
+        var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+        var migration = context.Database.GetPendingMigrations();
+        if (migration != null && migration.Any())
+        {
+            context.Database.Migrate();
+        }
+    }
+    catch (Exception ex)
+    {
+        Debug.WriteLine($"Error : Program, Migration of database :: {ex.Message}");
+    }
+}
+
+
+
 
 host.Run();
